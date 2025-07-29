44cffcb70553bc4da31953df795732f92b7d02b8
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2025-07-29 12:19:57 -0400

    working version

--- src/traccuracy/metrics/_chota.py
+++ src/traccuracy/metrics/_chota.py
@@ -1,11 +1,15 @@
+import logging
 import warnings
 from collections import Counter
 
 import networkx as nx
 import numpy as np
 
+from traccuracy._tracking_graph import NodeFlag
 from traccuracy.matchers._base import Matched
 from traccuracy.metrics._base import MATCHING_TYPES, Metric
+
+LOG = logging.getLogger(__name__)
 
 
 def _tracklets_graph(
@@ -58,9 +62,10 @@ def _assign_trajectories(
     tracklet_assignments = [np.asarray([n], dtype=int) for n in tracklets_graph.nodes]
 
     for tracklet in tracklets_graph.nodes:
-        trajectory_tracklets = list(nx.bfs_tree(tracklets_graph, tracklet).nodes) + list(
-            nx.bfs_tree(tracklets_graph, tracklet, reverse=True).nodes
-        )
+        trajectory_tracklets = (
+            list(nx.bfs_tree(tracklets_graph, tracklet).nodes)
+            + list(nx.bfs_tree(tracklets_graph, tracklet, reverse=True).nodes)[1:]
+        )  # avoiding including self twice
         tracklet_assignments[tracklet] = np.asarray(trajectory_tracklets)
 
     return tracklet_assignments
@@ -129,18 +134,16 @@ class CHOTAMetric(Metric):
         fn = len(matched.gt_graph.nodes) - len(matched.mapping)
 
         # repeated predicted matching are counted as false positives
-        counter = Counter([pred_node_id for _, pred_node_id in matched.mapping])
-        for v in counter.values():
-            if v > 1:
-                fp += v - 1
+        pred_counter = Counter([pred_node_id for _, pred_node_id in matched.mapping])
+        for c in pred_counter.values():
+            if c > 1:
+                fp += c - 1
 
         # repeated ground truth matching are counted as false negatives
-        counter = Counter([gt_node_id for gt_node_id, _ in matched.mapping])
-        for v in counter.values():
-            if v > 1:
-                fn += v - 1
-
-        print(f"{fp=} {fn=}")
+        gt_counter = Counter([gt_node_id for gt_node_id, _ in matched.mapping])
+        for c in gt_counter.values():
+            if c > 1:
+                fn += c - 1
 
         tracklets_overlap = np.zeros(
             (len(pred_tracklets), len(gt_tracklets)),
@@ -150,16 +153,36 @@ class CHOTAMetric(Metric):
         pred_tracklet_mask = np.zeros_like(tracklets_overlap, dtype=bool)
         gt_tracklet_mask = np.zeros_like(tracklets_overlap, dtype=bool)
 
+        tracklets_fp = np.zeros(len(pred_tracklets), dtype=int)
+        tracklets_fn = np.zeros(len(gt_tracklets), dtype=int)
+
+        fp_nodes = matched.pred_graph.get_nodes_with_flag(NodeFlag.CTC_FALSE_POS)
+        for node in fp_nodes:
+            pred_track_id = pred_track_ids[node]
+            tracklets_fp[pred_track_id] += 1
+
+        fn_nodes = matched.gt_graph.get_nodes_with_flag(NodeFlag.CTC_FALSE_NEG)
+        for node in fn_nodes:
+            gt_track_id = gt_track_ids[node]
+            tracklets_fn[gt_track_id] += 1
+
         for gt_node, pred_node in matched.mapping:
             pred_track_id = pred_track_ids[pred_node]
             gt_track_id = gt_track_ids[gt_node]
             tracklets_overlap[pred_track_id, gt_track_id] += 1
+
+        LOG.info("tracklets_overlap.sum()={}", tracklets_overlap.sum().item())
+        LOG.info("tracklets_overlap.shape={}", tracklets_overlap.shape)
+
+        # TODO:
+        # invert the matrix, iterating over rows as GTs should be better
 
         total_A_sigma = 0
         for i in range(len(pred_tracklets)):
             pred_tracklet_mask.fill(False)
             pred_tracklet_mask[pred_tracklet_assignments[i], :] = True
             pred_overlap_sum = tracklets_overlap[pred_tracklet_assignments[i], :].sum()
+            pred_overlap_sum += tracklets_fp[pred_tracklet_assignments[i]].sum()
 
             for j in np.nonzero(tracklets_overlap[i, :])[0]:
                 gt_tracklet_mask.fill(False)
@@ -167,18 +190,35 @@ class CHOTAMetric(Metric):
 
                 tpa = tracklets_overlap[pred_tracklet_mask & gt_tracklet_mask].sum()
                 fpa = pred_overlap_sum - tpa
-                fna = tracklets_overlap[:, gt_tracklet_assignments[j]].sum() - tpa
+
+                gt_overlap_sum = tracklets_overlap[:, gt_tracklet_assignments[j]].sum()
+                gt_overlap_sum += tracklets_fn[gt_tracklet_assignments[j]].sum()
+                fna = gt_overlap_sum - tpa
 
                 tpa, fpa, fna = tpa.item(), fpa.item(), fna.item()
-                print(f"{tpa=} {fpa=} {fna=}")
+
+                LOG.info(
+                    "tracklets_overlap[i, j]={}, pred_overlap_sum={} gt_overlap_sum={}",
+                    tracklets_overlap[i, j].item(),
+                    pred_overlap_sum.item(),
+                    gt_overlap_sum.item(),
+                )
+                LOG.info(
+                    "tpa={} fpa={} fna={} pred_tracklet_size={} gt_tracklet_size={}",
+                    tpa,
+                    fpa,
+                    fna,
+                    len(pred_tracklet_assignments[i]),
+                    len(gt_tracklet_assignments[j]),
+                )
 
                 A_sigma = tracklets_overlap[i, j] * tpa / (tpa + fpa + fna)
                 total_A_sigma += A_sigma
 
         union = fp + fn + len(matched.mapping)
 
-        print(f"{fp=} {fn=} {len(matched.mapping)=}")
-        print(total_A_sigma, union)
+        LOG.info("fp={} fn={} len(matched.mapping)={}", fp, fn, len(matched.mapping))
+        LOG.info("total_A_sigma={} union={}", total_A_sigma, union)
 
         return {
             "CHOTA": np.sqrt(total_A_sigma / union).item(),
