3c8777d5ba6499a8c51b6b821e81563d620ccbd0
Jordão Bragantini <jordao.bragantini@czbiohub.org>
2025-07-30 11:32:59 -0400

    adding CHOTA metric (#274)
    If you are implementing a new matcher or metric, please append this
    `&template=new_matcher_metric.md` to your url to load the correct
    template.
    
    # Proposed Change
    Adding CHOTA metric, reference https://arxiv.org/pdf/2408.11571
    
    # Types of Changes
    - New feature or enhancement
    
    Which topics does your change affect? Delete those that do not apply.
    - Metrics
    
    # Checklist
    Put an x in the boxes that apply. You can also fill these out after
    creating the PR. If you're unsure about any of them, don't hesitate to
    ask. We're here to help! This is simply a reminder of what we are going
    to look for before merging your code.
    
    - [ ] I have read the developer/contributing docs.
    - [ ] I have added tests that prove that my feature works in various
    situations or tests the bugfix (if appropriate).
    - [ ] I have checked that I maintained or improved code coverage.
    - [ ] I have checked the benchmarking action to verify that my changes
    did not adversely affect performance.
    - [ ] I have written docstrings and checked that they render correctly
    in the Read The Docs build (created after the PR is opened).
    - [ ] I have updated the general documentation including Metric
    descriptions and example notebooks if necessary.
    
    # Further Comments
    Any suggestion on how to test this?
    
    ---------
    
    Co-authored-by: Morgan Schwartz <msschwartz21@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/traccuracy/metrics/__init__.py
+++ src/traccuracy/metrics/__init__.py
@@ -1,4 +1,5 @@
 from ._basic import BasicMetrics
+from ._chota import CHOTAMetric
 from ._ctc import AOGMMetrics, CellCycleAccuracy, CTCMetrics
 from ._divisions import DivisionMetrics
 from ._results import Results
@@ -7,6 +8,7 @@ from ._track_overlap import TrackOverlapMetrics
 __all__ = [
     "AOGMMetrics",
     "BasicMetrics",
+    "CHOTAMetric",
     "CTCMetrics",
     "CellCycleAccuracy",
     "DivisionMetrics",
--- tests/bench.py
+++ tests/bench.py
@@ -16,6 +16,7 @@ from traccuracy.loaders._point import load_point_data
 from traccuracy.matchers import CTCMatcher, IOUMatcher, PointMatcher, PointSegMatcher
 from traccuracy.metrics import (
     BasicMetrics,
+    CHOTAMetric,
     CTCMetrics,
     DivisionMetrics,
     TrackOverlapMetrics,
@@ -311,5 +312,19 @@ def test_cca_metric(benchmark, iou_matched, request):
 
     def run_compute():
         return CellCycleAccuracy().compute(matched)
+
+    benchmark.pedantic(run_compute, rounds=1, iterations=1)
+
+
+@pytest.mark.parametrize(
+    "ctc_matched",
+    ["ctc_matched_2d", "ctc_matched_3d"],
+    ids=["2d", "3d"],
+)
+def test_chota_metric(benchmark, ctc_matched, request):
+    ctc_matched = request.getfixturevalue(ctc_matched)
+
+    def run_compute():
+        return CHOTAMetric().compute(ctc_matched)
 
     benchmark.pedantic(run_compute, rounds=1, iterations=1)
