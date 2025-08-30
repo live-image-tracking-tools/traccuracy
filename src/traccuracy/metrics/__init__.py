8bc2edbc7db19ab21a3153a68ae047aa65c9928e
Morgan Schwartz <msschwartz21@gmail.com>
2025-08-30 16:58:43 -0400

    Move CellCycleAccuracy out of ctc submodule into stand alone submodule -- wip typing issue

--- src/traccuracy/metrics/__init__.py
+++ src/traccuracy/metrics/__init__.py
@@ -1,6 +1,7 @@
 from ._basic import BasicMetrics
+from ._cca import CellCycleAccuracy
 from ._chota import CHOTAMetric
-from ._ctc import AOGMMetrics, CellCycleAccuracy, CTCMetrics
+from ._ctc import AOGMMetrics, CTCMetrics
 from ._divisions import DivisionMetrics
 from ._results import Results
 from ._track_overlap import TrackOverlapMetrics
