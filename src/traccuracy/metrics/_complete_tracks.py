8f759423780788afedbfe4ee3ca7e44517f95fa6
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2025-09-09 10:27:55 -0400

    fix: :bug: Test division edges and fix bugs related to checking their correctness
    Two bugs: I didn't check the division edge correctness for the lineage correctness, and WrongSemantic should be checked on the pred graph, not the GT graph. Also improved the docstrings.

--- src/traccuracy/metrics/_complete_tracks.py
+++ src/traccuracy/metrics/_complete_tracks.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import itertools
 import warnings
 from typing import TYPE_CHECKING
 
@@ -15,7 +16,6 @@ from ._base import Metric
 if TYPE_CHECKING:
     from collections.abc import Hashable
 
-    from traccuracy import TrackingGraph
     from traccuracy.matchers import Matched
 
 
@@ -23,7 +23,8 @@ class CompleteTracks(Metric):
     """The fraction of tracklets and lineages that are completely correctly reconstructed.
 
     If the reconstruction continues beyond the ground truth track, this is NOT
-    counted as incorrect.
+    counted as incorrect, nor are false positive tracks penalized, making this suitable
+    for evaluating with sparse ground truth annotations.
 
     If a False Positive Division occurs within the ground truth track (or, for the CTC
     errors, a wrong semantic edge), this IS counted as incorrect.
@@ -44,8 +45,10 @@ class CompleteTracks(Metric):
     def _compute(
         self, matched: Matched, relax_skips_gt: bool = False, relax_skips_pred: bool = False
     ) -> dict:
-        """The compute methods of Metric objects return a dictionary with counts and statistics.
-        Currently expects either CTC error annotation or basic errors + division errors.
+        """Computes the fraction of fully correct tracklets and lineages in the matched object.
+
+        If skip edges are relaxed in one graph, then skip_tp edges in the other graph are
+        counted as correct, along with nodes between the skip_tp edges in that graph.
 
         Args:
             matched (traccuracy.matchers.Matched): Matched data object to compute metrics on
@@ -54,11 +57,20 @@ class CompleteTracks(Metric):
             relax_skips_pred (bool): If True, the metric will check if skips in the predicted
                 graph have an equivalent multi-edge path in ground truth graph
 
-        Raises:
-            NotImplementedError
-
         Returns:
-            dict: Dictionary of metric names and int/float values
+            dict: A results dictionary with the following entries:
+                - `total_lineages` - the number of connected components in the ground truth graph
+                - `correct_lineages` - the number of fully correct connected components
+                - `complete_lineages` - `correct_lineages` / `total_lineages`, or np.nan if
+                    `total_lineages` is 0
+                - `total_tracklets` - the number of tracklets in the ground truth graph, defined
+                    as the connected components of the graph after division edges are removed.
+                    Division edges are not included in the tracklets, or counted at all
+                    in the tracklet metrics.
+                - `correct_tracklets` - the number of fully correct tracklets
+                - `complete_tracklets` - `correct_tracklets` / `total_tracklets`, or np.nan if
+                    `total_tracklets` is 0
+
         """
         if self.error_type == "basic":
             classify_basic_errors(
@@ -83,6 +95,7 @@ class CompleteTracks(Metric):
         lineage_starts = [node for node, in_degree in gt_nxgraph.in_degree() if in_degree == 0]  # type: ignore
         for lineage_start in lineage_starts:
             tracklet_starts = [lineage_start]
+            div_edges = []
             curr_nodes = [lineage_start]
             while len(curr_nodes) > 0:
                 next_succs = []
@@ -91,6 +104,7 @@ class CompleteTracks(Metric):
                     next_succs.extend(daughters)
                     if len(daughters) == 2:
                         tracklet_starts.extend(daughters)
+                        div_edges.extend([(succ, daught) for daught in daughters])
                 curr_nodes = next_succs
 
             subtracklets_correct = [
@@ -102,11 +116,20 @@ class CompleteTracks(Metric):
                 )
                 for tracklet_start in tracklet_starts
             ]
+            div_edges_correct = [
+                self._check_gt_edge_correct(
+                    div_edge,
+                    matched,
+                    relax_skips_gt=relax_skips_gt,
+                    relax_skips_pred=relax_skips_pred,
+                )
+                for div_edge in div_edges
+            ]
+            lineage_correct = all(subtracklets_correct) and all(div_edges_correct)
             total_tracklets += len(tracklet_starts)
             correct_tracklets += sum(subtracklets_correct)
             total_lineages += 1
-            if all(subtracklets_correct):
-                correct_lineages += 1
+            correct_lineages += lineage_correct
 
         return {
             "total_lineages": total_lineages,
@@ -129,9 +152,7 @@ class CompleteTracks(Metric):
         out_edges = list(matched.gt_graph.graph.out_edges(start_node))
         while len(out_edges) == 1:
             out_edge = out_edges[0]
-            if not self._check_gt_edge_correct(
-                out_edge, matched.gt_graph, relax_skips_gt, relax_skips_pred
-            ):
+            if not self._check_gt_edge_correct(out_edge, matched, relax_skips_gt, relax_skips_pred):
                 return False
             curr_node = out_edge[1]
             if not self._check_gt_node_correct(
@@ -173,21 +194,33 @@ class CompleteTracks(Metric):
     def _check_gt_edge_correct(
         self,
         edge: tuple[Hashable, Hashable],
-        gt_track: TrackingGraph,
+        matched: Matched,
         relax_skips_gt: bool,
         relax_skips_pred: bool,
     ) -> bool:
+        gt_track = matched.gt_graph
+        pred_track = matched.pred_graph
         edge_data = gt_track.edges[edge]
         # check if it is a TP
         if self.error_type == "ctc":
             # the ctc errors don't annotate edge TPs, so instead we check for absence of
-            # all the error types
+            # all the error types. Wrong semantic are only annotated on the pred graph,
+            # so we need to find the matched edge and check it
             tp = True
-            errors = [EdgeFlag.CTC_FALSE_NEG, EdgeFlag.WRONG_SEMANTIC]
-            for error in errors:
-                if error in edge_data:
-                    tp = False
-                    break
+            if EdgeFlag.CTC_FALSE_NEG in edge_data:
+                tp = False
+            else:
+                matched_sources = matched.get_gt_pred_matches(edge[0])
+                matched_targets = matched.get_gt_pred_matches(edge[1])
+                matched_edges = [
+                    (source, target)
+                    for source, target in itertools.product(matched_sources, matched_targets)
+                    if pred_track.graph.has_edge(source, target)
+                ]
+                for matched_edge in matched_edges:
+                    if EdgeFlag.WRONG_SEMANTIC in pred_track.graph.edges[matched_edge]:
+                        tp = False
+                        break
             if tp:
                 return True
         else:
--- tests/metrics/test_complete_tracks.py
+++ tests/metrics/test_complete_tracks.py
@@ -26,25 +26,30 @@ expected_results_two_to_one = [
     (ex_graphs.edge_two_to_one, (2, 0, 0)),
 ]
 
-expected_skip_edge_cases_no_relax = [
-    (ex_graphs.gap_close_gt_gap, (1, 0, 0)),
-    (ex_graphs.gap_close_pred_gap, (1, 0, 0)),
-    (ex_graphs.gap_close_matched_gap, (1, 1, 1)),
-    (ex_graphs.gap_close_offset, (1, 0, 0)),
+expected_skip_edge_cases = [
+    # matched, relax_gt, relax_pred, (total, correct, fraction)
+    (ex_graphs.gap_close_gt_gap, False, False, (1, 0, 0)),
+    (ex_graphs.gap_close_pred_gap, False, False, (1, 0, 0)),
+    (ex_graphs.gap_close_matched_gap, False, False, (1, 1, 1)),
+    (ex_graphs.gap_close_offset, False, False, (1, 0, 0)),
+    (ex_graphs.gap_close_gt_gap, True, False, (1, 1, 1)),
+    (ex_graphs.gap_close_pred_gap, True, False, (1, 0, 0)),
+    (ex_graphs.gap_close_matched_gap, True, False, (1, 1, 1)),
+    (ex_graphs.gap_close_offset, True, False, (1, 0, 0)),
+    (ex_graphs.gap_close_gt_gap, False, True, (1, 0, 0)),
+    (ex_graphs.gap_close_pred_gap, False, True, (1, 1, 1)),
+    (ex_graphs.gap_close_matched_gap, False, True, (1, 1, 1)),
+    (ex_graphs.gap_close_offset, False, True, (1, 0, 0)),
 ]
 
-expected_skip_edge_cases_relax_gt = [
-    (ex_graphs.gap_close_gt_gap, (1, 1, 1)),
-    (ex_graphs.gap_close_pred_gap, (1, 0, 0)),
-    (ex_graphs.gap_close_matched_gap, (1, 1, 1)),
-    (ex_graphs.gap_close_offset, (1, 0, 0)),
-]
-
-expected_skip_edge_cases_relax_pred = [
-    (ex_graphs.gap_close_gt_gap, (1, 0, 0)),
-    (ex_graphs.gap_close_pred_gap, (1, 1, 1)),
-    (ex_graphs.gap_close_matched_gap, (1, 1, 1)),
-    (ex_graphs.gap_close_offset, (1, 0, 0)),
+expected_div_cases = [
+    # matched, params, (total_lin, correct_lin, frac_lin, total_tra, correct_tra, fraction_tra)
+    (ex_graphs.good_div, (0, 1, 2), (1, 1, 1, 3, 3, 1)),
+    (ex_graphs.fp_div, (0, 1), (2, 1, 0.5, 2, 1, 0.5)),
+    (ex_graphs.one_child, (0, 1), (1, 0, 0, 3, 3, 1)),
+    (ex_graphs.no_children, (0, 1), (1, 0, 0, 3, 3, 1)),
+    (ex_graphs.wrong_child, (0, 1), (2, 1, 0.5, 4, 3, 0.75)),
+    (ex_graphs.wrong_children, (0, 1), (1, 0, 0, 3, 1, 1 / 3)),
 ]
 
 
@@ -117,18 +122,21 @@ def test_two_to_one(matched_func, expected_result):
     "ignore:Node errors already calculated",
     "ignore:Edge errors already calculated",
 )
-@pytest.mark.parametrize("matched_func,expected_result", expected_skip_edge_cases_no_relax)
-def test_skip_edges(matched_func, expected_result):
+@pytest.mark.parametrize(
+    "matched_func,relax_gt,relax_pred,expected_result", expected_skip_edge_cases
+)
+def test_skip_edges(matched_func, relax_gt, relax_pred, expected_result):
     # only valid on basic
     complete_tracks = CompleteTracks(error_type="basic")
     matched = matched_func()
-    result = complete_tracks.compute(matched)
-    assert result.results["total_tracklets"] == expected_result[0]
-    assert result.results["correct_tracklets"] == expected_result[1]
-    if expected_result[2] is np.nan:
+    result = complete_tracks.compute(matched, relax_skips_gt=relax_gt, relax_skips_pred=relax_pred)
+    total, correct, fraction = expected_result
+    assert result.results["total_tracklets"] == total
+    assert result.results["correct_tracklets"] == correct
+    if fraction is np.nan:
         assert result.results["complete_tracklets"] is np.nan
     else:
-        assert result.results["complete_tracklets"] == expected_result[2]
+        assert result.results["complete_tracklets"] == fraction
 
 
 @pytest.mark.filterwarnings(
@@ -136,37 +144,33 @@ def test_skip_edges(matched_func, expected_result):
     "ignore:Node errors already calculated",
     "ignore:Edge errors already calculated",
 )
-@pytest.mark.parametrize("matched_func,expected_result", expected_skip_edge_cases_relax_gt)
-def test_skip_edges_relax_gt(matched_func, expected_result):
-    # only valid on basic
-    complete_tracks = CompleteTracks(error_type="basic")
-    matched = matched_func()
-    result = complete_tracks.compute(matched, relax_skips_gt=True)
-    assert result.results["total_tracklets"] == expected_result[0]
-    assert result.results["correct_tracklets"] == expected_result[1]
-    if expected_result[2] is np.nan:
-        assert result.results["complete_tracklets"] is np.nan
-    else:
-        assert result.results["complete_tracklets"] == expected_result[2]
+@pytest.mark.parametrize("error_type", ["basic", "ctc"])
+@pytest.mark.parametrize("matched_func,params,expected_result", expected_div_cases)
+def test_div_examples(error_type, matched_func, params, expected_result):
+    complete_tracks = CompleteTracks(error_type=error_type)
+    for param in params:
+        print(param)
+        matched = matched_func(param)
+        result = complete_tracks.compute(matched)
+        for edge in matched.gt_graph.edges:
+            print(edge, matched.gt_graph.graph.edges[edge])
+        for edge in matched.pred_graph.edges:
+            print(edge, matched.pred_graph.edges[edge])
+        total_lin, correct_lin, fraction_lin, total_tra, correct_tra, fraction_tra = expected_result
 
+        assert result.results["total_tracklets"] == total_tra
+        assert result.results["correct_tracklets"] == correct_tra
+        if fraction_tra is np.nan:
+            assert result.results["complete_tracklets"] is np.nan
+        else:
+            assert result.results["complete_tracklets"] == fraction_tra
 
-@pytest.mark.filterwarnings(
-    "ignore:Mapping is empty",
-    "ignore:Node errors already calculated",
-    "ignore:Edge errors already calculated",
-)
-@pytest.mark.parametrize("matched_func,expected_result", expected_skip_edge_cases_relax_pred)
-def test_skip_edges_relax_pred(matched_func, expected_result):
-    # only valid on basic
-    complete_tracks = CompleteTracks(error_type="basic")
-    matched = matched_func()
-    result = complete_tracks.compute(matched, relax_skips_pred=True)
-    assert result.results["total_tracklets"] == expected_result[0]
-    assert result.results["correct_tracklets"] == expected_result[1]
-    if expected_result[2] is np.nan:
-        assert result.results["complete_tracklets"] is np.nan
-    else:
-        assert result.results["complete_tracklets"] == expected_result[2]
+        assert result.results["total_lineages"] == total_lin
+        assert result.results["correct_lineages"] == correct_lin
+        if fraction_lin is np.nan:
+            assert result.results["complete_lineages"] is np.nan
+        else:
+            assert result.results["complete_lineages"] == fraction_lin
 
 
 @pytest.mark.filterwarnings(
