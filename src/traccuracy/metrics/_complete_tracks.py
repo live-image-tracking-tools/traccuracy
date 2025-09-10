eed542831f57d0845aefca8c520b7f46330da31a
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2025-09-10 17:22:38 -0400

    Shorten comment for ruff

--- src/traccuracy/metrics/_complete_tracks.py
+++ src/traccuracy/metrics/_complete_tracks.py
@@ -96,7 +96,7 @@ class CompleteTracks(Metric):
         gt_nxgraph = matched.gt_graph.graph
         lineage_starts = [node for node, in_degree in gt_nxgraph.in_degree() if in_degree == 0]  # type: ignore
         for lineage_start in lineage_starts:
-            # Within each lineage, find all division edges and daughters that are the start of tracklets
+            # Within each lineage, find all division edges and daughters that start tracklets
             tracklet_starts = [lineage_start]
             div_edges = []
             curr_nodes = [lineage_start]
